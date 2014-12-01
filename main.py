90135daf5e30bf5ae6a55a578087042e281f8938
parpanironak <parpani.ronak@gmail.com>
2014-12-01 01:24:11 -0500

    more and less options added

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ Created on Nov 30, 2014
 
 import os
 from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext import ndb
+from google.appengine.datastore.datastore_query import Cursor
 
 import jinja2
 import webapp2
@@ -20,7 +22,7 @@ def creator_key(creator_ID):
     return ndb.Key('Creator',creator_ID)
 
 def question_key(question_ID):
-    return ndb.Key('Question',question_ID)
+    return db.Key('Question',question_ID)
 
 def answer_key(answer_ID):
     return ndb.Key('Answer',answer_ID)
@@ -36,6 +38,11 @@ def qvote_key(qvote_ID):
 class Question(ndb.Model):
     creator = ndb.StringProperty(indexed=True)
     qtitle = ndb.StringProperty(indexed=True)
+    qdetail = ndb.StringProperty(indexed=False)
+    votecount = ndb.IntegerProperty(indexed=True)
+    creationdatetime = ndb.DateTimeProperty(auto_now_add=True)
+    date = ndb.DateProperty(auto_now_add=True)
+    time = ndb.TimeProperty(auto_now_add=True)
 
  
 class Answer(ndb.Model):
@@ -66,29 +73,89 @@ class Main(webapp2.RedirectHandler):
     def get(self):
         user = users.get_current_user()
         
-        guest = None
+        isguest = None
         username = None
         url = None
         if user:
-            guest = False
+            isguest = False
             username = user.nickname()
+            url = users.create_logout_url('/')
         else:
-            guest = True
+            isguest = True
             username = "guest"
             url = users.create_login_url("/")
+        
+        
+        self.query = Question.query().order(Question.creationdatetime);
+        curs = Cursor(urlsafe=self.request.get('cursor'))        
+        prevcurs = self.request.get('prevcursor')        
+        qlist, next_curs, more = self.query.fetch_page(5, start_cursor=curs)
+        
+        
+        nextlink = None        
+        prevlink = None
+        less = False;
+        
+        if prevcurs:
+            less = True;
+            prevlink = "/?cursor=" + prevcurs
+            
+        elif self.request.get('cursor'):
+            less = True;
+            prevlink = "/"
+                
+        if next_curs:
+            nextlink = "/?cursor=" + next_curs.urlsafe() + "&prevcursor=" +curs.urlsafe()
+        
+        else:
+            more=False;
+        
         template_values = {
             'user' : username,
-            'guest' : guest,
-            'url' : url
+            'isguest' : isguest,
+            'url' : url,
+            'qlist': qlist,
+            'more': nextlink,
+            'ismore': more,
+            'isless': less,
+            'less': prevlink,
         }
-        template = JINJA_ENVIRONMENT.get_template("temp2.html")
+        template = JINJA_ENVIRONMENT.get_template("temp3.html")
         self.response.write(template.render(template_values))
         
     def post(self):
         
-        a = 10
+        user = users.get_current_user();
+        if user:
+            self.question = Question(creator=user.nickname(), qtitle=self.request.get('comment'), votecount=0)        
+            self.question.put();
+            self.redirect(self.request.referer)
+        else:
+            url = users.create_login_url("/")
+            self.redirect(url)
+
+class QuestionPage:
+    def get(self):
+        user = users.get_current_user()
+        
+        isguest = None
+        username = None
+        url = None
+        if user:
+            isguest = False
+            username = user.nickname()
+            url = users.create_logout_url('/')
+        else:
+            isguest = True
+            username = "guest"
+            url = users.create_login_url("/")  
+            
+        
 
 
+    
+        
 app = webapp2.WSGIApplication([
-       ('/',Main),                       
+       ('/',Main),
+       ('/', QuestionPage)                       
     ], debug=True)
